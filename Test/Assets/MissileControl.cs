88e6ed024a5c71d845df71fd95fa9bb518bc941b
Nakkwan <cvvc1997@gmail.com>
2020-10-18 00:32:32 +0900

    Add data file, Set LOS Simulation

--- Test/Assets/MissileControl.cs
+++ Test/Assets/MissileControl.cs
@@ -12,10 +12,14 @@ public class MissileControl : MonoBehaviour
     public float ini_vel;
     public float HEDEG;
     public double time;
-    string filename;
+    public float LOS_length;
+    string pos_filename;
+    string los_filename;
     int idx;
     int interval;
     List<List<double>> Missile_Pos;
+    List<List<double>> LOS;
+    private LineRenderer LineDrawer;
 
     void csvRead_Pos(string filename, List<List<double>> output)
     {
@@ -30,8 +34,10 @@ public class MissileControl : MonoBehaviour
             string[] data = s.Split(',');
 
             output.Add(new List<double>());
-            output[n].Add(Convert.ToDouble(data[0]));
-            output[n].Add(Convert.ToDouble(data[1]));
+            for (int i = 0; i < data.Length; i++)
+            {
+                output[n].Add(Convert.ToDouble(data[i]));
+            }
             n++;
         }
     }
@@ -40,14 +46,18 @@ public class MissileControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.filename = "Missile_Traj.txt";
+        this.pos_filename = "Missile_Traj.txt";
+        this.los_filename = "Line_of_Sight.txt";
         interval = (int)(Time.deltaTime / time);
         this.idx = 0;
         Missile_Pos = new List<List<double>>();
+        LOS = new List<List<double>>();
+        LineDrawer = GetComponent<LineRenderer>();
 
         //this.rigid.velocity = new Vector3(ini_vel * Mathf.Sin(HEDEG), ini_vel * Mathf.Cos(HEDEG), 0);
 
-        csvRead_Pos(this.filename, this.Missile_Pos);
+        csvRead_Pos(this.pos_filename, this.Missile_Pos);
+        csvRead_Pos(this.los_filename, this.LOS);
 
         Debug.Log(this.Missile_Pos.Count);
         Debug.Log(this.Missile_Pos[0].Count);
@@ -59,7 +69,14 @@ public class MissileControl : MonoBehaviour
     {
         transform.position = new Vector3((float)Missile_Pos[idx][1], (float)Missile_Pos[idx][0] - 2000, 0);
         //transform.Translate((float)(Missile_Pos[idx][1] - Missile_Pos[idx - interval][1]), (float)(Missile_Pos[idx][0] - Missile_Pos[idx - interval][0]), 0);
-        if (idx + interval < this.Missile_Pos.Count)
+        LineDrawer.positionCount = 2;
+        LineDrawer.startWidth = 10;
+        LineDrawer.endWidth = 10;
+        LineDrawer.startColor = Color.red;
+        LineDrawer.endColor = Color.red;
+        LineDrawer.SetPosition(0, transform.position);
+        LineDrawer.SetPosition(1, new Vector3(transform.position.x + LOS_length * Mathf.Sin((float)LOS[idx][0] * Mathf.PI / 180), transform.position.y + LOS_length * Mathf.Cos((float)LOS[idx][0] * Mathf.PI / 180), transform.position.z));
+        if (idx + interval < this.Missile_Pos.Count && idx + interval < this.LOS.Count)
         {
             idx += interval;
         }
